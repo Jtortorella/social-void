37cfe17d6371e5263059927bc75669864564fd44
jtortorella <jwtortorella@gmail.com>
2025-12-10 10:51:36 -0500

    Deleted rogue packagelock and nodemodules.

---
+++ website/src/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ComingSoon.css";
+
+export default function ComingSoon() {
+  return (
+    <div className="coming-soon-container">
+      <h1 className="coming-soon-title">Coming Soon</h1>
+      <p className="coming-soon-text">
+        We're working hard to bring you something amazing.
+      </p>
+    </div>
+  );
+}
