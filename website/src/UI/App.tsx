9977c5177255f6323ebdba63d3291f1b5ac83463
jtortorella <jwtortorella@gmail.com>
2025-12-10 11:04:48 -0500

    Commenting out imports, causing errors.

--- website/src/UI/App.tsx
+++ website/src/UI/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { lazy } from "react";
 const ComingSoon = lazy(() => import('../UI/ComingSoon/ComingSoon'))
-const Youtube = lazy(() => import('../APIs/Youtube/Youtube'));
-const Items = lazy(() => import('../APIs/BandCamp/Items'));
-const Spotify = lazy(() => import('../APIs/Spotify/Spotify'));
-const Seated = lazy(() => import('../APIs/Seated/Seated'));
-const Navbar = lazy(() => import('./Navbar/Navbar'));
-const Footer = lazy(() => import('./Footer/Footer'));
-const Header = lazy(() => import('./Headers/Header'));
+// const Youtube = lazy(() => import('../APIs/Youtube/Youtube'));
+// const Items = lazy(() => import('../APIs/BandCamp/Items'));
+// const Spotify = lazy(() => import('../APIs/Spotify/Spotify'));
+// const Seated = lazy(() => import('../APIs/Seated/Seated'));
+// const Navbar = lazy(() => import('./Navbar/Navbar'));
+// const Footer = lazy(() => import('./Footer/Footer'));
+// const Header = lazy(() => import('./Headers/Header'));
 
 function App() {
   return (
