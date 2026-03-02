25063b3e11f09b4d45a1ee66c694b78277bb58c8
Jtortorella <jwtortorella@gmail.com>
2026-03-02 18:00:57 -0500

    Changes

--- website/src/UI/Footer/Footer.tsx
+++ website/src/UI/Footer/Footer.tsx
@@ -4,15 +4,17 @@ import "./Footer.scss";
 function Footer() {
   return (
     <nav className="footer-grid">
-      <div className="col">       
-        <IconBar width={30} height={30} className={""} />
-        All Contents © Social Void
-      </div>
-      <div className="col">
+            <div className="col">
         <p className="band-name">Social Void</p>
       </div>
       <div className="col">
           <a className="email" href="mailto:socialvoidband@gmail.com">Send Us An Email</a>
+      </div>
+
+
+            <div className="col">       
+        <IconBar width={30} height={30} className={""} />
+        All Contents © Social Void
       </div>
     </nav>
   );
