c88a83050289a7689584eacad4f15c7b0e36c943
Jtortorella <jwtortorella@gmail.com>
2026-03-02 18:35:00 -0500

    Updating image on smaller screens

--- website/src/UI/Hero/Hero.tsx
+++ website/src/UI/Hero/Hero.tsx
@@ -1,11 +1,15 @@
 import HorizontalBanner from "../../Assets/HorizontalBanner.png";
+import BandImage from "../../Assets/BandImage.png"
 import "./Hero.scss"
 export const Hero = () => {
     return (
         <div className="hero">
             <div className="hero-container">
-                <img className="hero-img"
+                <img className="hero-img horizontal"
                     src={HorizontalBanner}
+                ></img>
+                <img className="hero-img vertical"
+                    src={BandImage}
                 ></img>
             </div>
         </div>
